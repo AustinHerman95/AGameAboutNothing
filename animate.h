0178db7c1de35784f894ff88cbd894a2b4f5bd34
AustinHerman95 <austin.herman95@yahoo.com>
2016-09-29 12:45:02 -0400

    reverting from component design

--- animate.h
+++ animate.h
@@ -3,31 +3,26 @@
 #include "SFML\Graphics.hpp"
 #include <iostream>
 
-class Object_Animate
+class Animate
 {
 public:
-	Object_Animate();
+	Animate();
 	void setup(sf::Vector2i, int width, int height);
-	void animate();
-	void reset();
+	void walking();
+	void releaseDirection(sf::Vector2f);
+	void xDirection(int direction);
+	void yDirection(int direction);
+	void setDimensions(sf::Vector2i);
+	void setOffSet(int action);
 protected:
 	sf::IntRect spriteRect;
 	int offset;
 	int iterate;
-	std::vector<int> left;
-	std::vector<int> top;
+	int left[12];
+	int top[4];
 
 	//The whole sprite sheet
 	sf::Texture mTexture;
 	//The actual sprite, this is what the user sees
 	sf::Sprite mSprite;
 };
-class Moveable_Animate: public Object_Animate
-{
-public:
-	Moveable_Animate();
-	void releaseDirection(sf::Vector2f);
-	void xDirection(int direction);
-	void yDirection(int direction);
-	void setOffSet(int action);
-}
